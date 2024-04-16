63ce3094efe607fd4638c0e6005499d04e263169
Topu Saha <topusaha@Topus-MacBook-Air.local>
2024-04-16 13:34:17 -0400

    Created Project

---
+++ NutriTrack/NutriTrack/NutriTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriTrackApp.swift
+//  NutriTrack
+//
+//  Created by Topu Saha on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NutriTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
